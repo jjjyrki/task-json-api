801ec9facdc444376a3ab7433ad1ce5d35af87ec
Jyri Vähä-Pietilä <jyrivp@gmail.com>
2019-10-09 21:40:47 +0300

    fix migration to include primary key

--- priv/repo/migrations/20191009181432_create_users.exs
+++ priv/repo/migrations/20191009181432_create_users.exs
@@ -2,7 +2,7 @@ defmodule TaskApi.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users, primary_key: false) do
+    create table(:users) do
       add :email, :string, null: false
       add :password_hash, :string
 
