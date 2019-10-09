23a8abb6db5babb286db6a438e70d6a6c9c51ea2
Jyri Vähä-Pietilä <jyrivp@gmail.com>
2019-10-09 21:30:33 +0300

    add password to user

--- lib/task_api/auth/user.ex
+++ lib/task_api/auth/user.ex
@@ -2,19 +2,30 @@ defmodule TaskApi.Auth.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
   schema "users" do
     field :email, :string
-
+    field :password, :string, virtual: true
+    field :password_hash, :string
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email])
+    |> cast(attrs, [:email, :password])
     |> validate_required([:email])
     |> unique_constraint(:email)
+    |> put_password_hash()
   end
+
+  defp put_password_hash(
+        %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+      ) do
+    change(changeset, password_hash: Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset) do
+    changeset
+  end
+
 end
