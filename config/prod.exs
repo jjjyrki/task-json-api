e1b9222c88c7029dcb94cd897b34e7c795f5056a
Jyri Vähä-Pietilä <jyrivp@gmail.com>
2019-10-27 23:12:37 +0200

    update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :task_api, TaskApiWeb.Endpoint,
   http: [port: 8888],
-  #url: [host: "example.com", port: 80],
+  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -56,7 +56,7 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+     config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
