157a3ff0ff5f48575be48c2230c424aefc9afbc9
Jyri Vähä-Pietilä <jyrivp@gmail.com>
2019-10-28 21:58:43 +0200

    try starting server differently

--- config/prod.exs
+++ config/prod.exs
@@ -56,12 +56,12 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-     config :phoenix, :serve_endpoints, true
+    # config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
 #
-#     config :task_api, TaskApiWeb.Endpoint, server: true
+     config :task_api, TaskApiWeb.Endpoint, server: true
 #
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
