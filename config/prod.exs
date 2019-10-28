308ca35bf62540641cb717c16aa06a9903760419
Jyri Vähä-Pietilä <jyrivp@gmail.com>
2019-10-28 23:39:37 +0200

    change port back to 8888

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :task_api, TaskApiWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 8888],
   url: [host: "example.com", port: 80]
   #cache_static_manifest: "priv/static/cache_manifest.json"
 
